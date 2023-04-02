b59456663c7576ac0304f69d3ea2f53f2c14aaa3
Dylan Gellis <dgellis@dylans-mbp.myfiosgateway.com>
2023-04-02 17:40:40 -0400

    Fixed copy bug?

--- src/components/shared/copy-text-area.tsx
+++ src/components/shared/copy-text-area.tsx
@@ -2,6 +2,32 @@ import {Button, Flex, Spacer, Textarea} from "@chakra-ui/react";
 import {CopyIcon} from "@chakra-ui/icons";
 import React from "react";
 
+async function copyToClipboard(textToCopy: string) {
+    // Navigator clipboard api needs a secure context (https)
+    if (navigator.clipboard && window.isSecureContext) {
+        await navigator.clipboard.writeText(textToCopy);
+    } else {
+        // Use the 'out of viewport hidden text area' trick
+        const textArea = document.createElement("textarea");
+        textArea.value = textToCopy;
+            
+        // Move textarea out of the viewport so it's not visible
+        textArea.style.position = "absolute";
+        textArea.style.left = "-999999px";
+            
+        document.body.prepend(textArea);
+        textArea.select();
+
+        try {
+            document.execCommand('copy');
+        } catch (error) {
+            console.error(error);
+        } finally {
+            textArea.remove();
+        }
+    };
+}
+
 export const CopyTextArea = ({ text } : { text : string }) => {
     return <>
         <Textarea
@@ -13,7 +39,10 @@ export const CopyTextArea = ({ text } : { text : string }) => {
         <Flex>
             <Spacer />
             <Button
-                onClick={() => navigator.clipboard.writeText(text)}
+                onClick={() => {
+                    console.log(text);
+                    copyToClipboard(text);
+                }}
                 colorScheme='blue'><CopyIcon paddingRight={1}/>Copy</Button>
         </Flex>
     </>
